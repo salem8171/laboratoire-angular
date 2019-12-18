c1401b2306413eaa405a170890034139dbeb67fe
youssefM95 <youssefmaaloul995@gmail.com>
2019-12-18 11:00:56 +0100

    added material data-table

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  current = 'home';
   constructor() { }
 
   ngOnInit() {
