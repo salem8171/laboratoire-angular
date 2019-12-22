d2c7f1f0fd522be6ec28e892721c7af47ba84cdd
youssefM95 <youssefmaaloul995@gmail.com>
2019-12-22 11:17:56 +0100

    added posts card with rate

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostService} from "../services/post.service";
+import {StarRatingComponent} from "ng-starrating";
 
 @Component({
   selector: 'app-post',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  posts: any;
+
+  constructor(private postService: PostService) {
+  }
 
   ngOnInit() {
+    this.getPosts();
+  }
+
+  getPosts() {
+    this.posts = this.postService.getPosts();
+  }
+
+  onRate($event: { oldValue: number, newValue: number, starRating: StarRatingComponent }) {
+    alert(`Old Value:${$event.oldValue}, 
+      New Value: ${$event.newValue}, 
+      Checked Color: ${$event.starRating.checkedcolor}, 
+      Unchecked Color: ${$event.starRating.uncheckedcolor}`);
   }
 
 }
